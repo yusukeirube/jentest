d546682240c4b435a16560f4f10563f88b2ff166
Yusuke Irube <yusuke.irube@dxc.com>
2023-03-27 15:16:35 +0900

    0102 戻す

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -9,7 +9,7 @@ export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
       { id: 12, name: 'Dr. Nice' },
-      { id: 13, name: 'Bombasto123' },
+      { id: 13, name: 'Bombasto' },
       { id: 14, name: 'Celeritas' },
       { id: 15, name: 'Magneta' },
       { id: 16, name: 'RubberMan' },
