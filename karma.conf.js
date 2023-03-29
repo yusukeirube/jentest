0accdda394cc2a4b5d7c9b0641715a12eccb3037
Yusuke Irube <yusuke.irube@dxc.com>
2023-03-29 17:00:34 +0900

    0201 karma.conf.js 修正してみた

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
